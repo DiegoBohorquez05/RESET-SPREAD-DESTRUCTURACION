4d7f343740794b147b909488e5245b21cc39072b
Osamacapa <majinantonio16@gmail.com>
2022-09-26 08:54:29 -0500

    actualizacion

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ console.log(sumar(1,2))
 console.log(sumar(5,6,4))
 console.log(sumar(20,30,40,50,60,70,80,90,100))
 
+//Funciones con parametros definidos
+
+function sumar3(a,b,z=2){
+    return sumar3=a+b+z
+}
+console.log(sumar3(1,2))
+//Si se da valor en z, esta se reemplaza
+
+//--------------------------------------------
+
 // Separar en datos individuales un grupo de valores
 
 const miEmpresa=["COEX","Conocimiento","Exponencial"]
@@ -20,6 +30,7 @@ console.log(sobreMiEmpresa)
 
 //Destructuracion
 
+//Arreglos
 const vec=[10,20,30]
 let numero1=vec[0]
 let numero2=vec[1]
@@ -31,6 +42,7 @@ console.log(num3)
 
 //-------------------------
 
+//Objetos
 const usuario={
     documento: 1098,
     nombre: "Max Steal",
@@ -40,3 +52,20 @@ const {documento,nombre,verificado}=usuario
 console.log(documento)
 console.log(nombre)
 console.log(verificado)
+
+//Recursion
+
+
+let num=+prompt(`Digite un numero`)
+
+const parImpar=(num)=>{
+    if(num==0){
+        return "par"
+    }else if(num==1){
+        return "impar"
+    }else{
+        return parImpar(num-2)
+    }
+}
+
+console.log(parImpar(num))
