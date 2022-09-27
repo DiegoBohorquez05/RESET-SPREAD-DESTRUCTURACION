ba39b4c5f753affdb5a40aaafb970f39c9a28108
Osamacapa <majinantonio16@gmail.com>
2022-09-27 06:55:36 -0500

    falto

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
-let tiempo=document.getElementById("dato")
+let tiempo=document.getElementById("dato").value;
 
 let temporizador =function(tiempo){
-    if(tiempo===0){
+    if(tiempo<=0){
         return "fin"
-    }else{
-        // return setTimeout(temporizador,1000,tiempo-1)
+    }else{ 
+        return setTimeout(temporizador,1000,tiempo-1)
     }
 }
 
 // let boton=document.getElementById("iniciar")
 // boton.addEventListener('click',temporizador)
 
-console.log(temporizador(0))
+console.log(temporizador(1))
+document.getElementById('contador').textContent=temporizador
